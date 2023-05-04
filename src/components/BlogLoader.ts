2fdfa7b119464ea0e61334ef5153fd256129c932
nnnnQT <cortana@s007xcpu2552.lan>
2023-05-03 22:55:57 -0500

    updated styles and components

--- src/components/BlogLoader.ts
+++ src/components/BlogLoader.ts
@@ -1,11 +1,15 @@
-import { Params } from "react-router-dom";
+import { Params, defer } from "react-router-dom";
 
 
 const BlogLoader = async (params: Params<string>) => {
     const {id} = params;
-    const blog = await fetch(`https://raw.githubusercontent.com/y04nqt/portfolio-data/main/blogs/${id}.json`)
-    const body = await blog.json();
-    return { blog: body };
+
+    const fetcher = async () => {
+        const data = await fetch(`https://raw.githubusercontent.com/y04nqt/portfolio-data/main/blogs/${id}.json`)
+        const body = await data.json();
+        return body;
+    }
+    return defer({ blog: fetcher() });
 }
 
 export default BlogLoader;
--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,23 +1,35 @@
-import { useLoaderData } from "react-router-dom";
+import { Await, Link, useLoaderData } from "react-router-dom";
 import CenteredWrapper from "./CenteredWrapper";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkBreaks from "remark-breaks";
+import React from "react";
 
 const BlogPost = () => {
     //@ts-ignore
     const { blog } = useLoaderData();
     return (
-        <CenteredWrapper>
-            <div className={`flex max-h-[calc(100%-61px)] bg-white rounded-xl shadow-xl appear-in`}>
-                <section className="flex flex-col p-10 overflow-y-auto text-left">
-                    <h1 className="m-0">{blog.title}</h1>
-                    <small>{blog.author}</small>
-                    <ReactMarkdown
-                        remarkPlugins={[remarkBreaks]}
-                    >{blog.body.replace(/<br>/gi, "&nbsp; \n")}</ReactMarkdown>
-                </section>
-            </div>
-        </CenteredWrapper>
+        <React.Suspense
+            fallback={<CenteredWrapper><p className="text-4xl font-thin text-center animate-pulse">Loading...</p></CenteredWrapper>}
+        >
+            <Await
+                resolve={blog}
+                errorElement={<CenteredWrapper><Link to="/"><p className="text-2xl text-red-800">Oh no! An error occured, click here to go back.</p></Link></CenteredWrapper>}
+            >
+                {(blog) =>(
+                    <CenteredWrapper>
+                        <div className={`flex max-h-[calc(100%-61px)] bg-white rounded-xl shadow-xl appear-in`}>
+                            <section className="flex flex-col p-10 overflow-y-auto text-left">
+                                <h1 className="m-0">{blog.title}</h1>
+                                <small>{blog.author}</small>
+                                <ReactMarkdown
+                                    remarkPlugins={[remarkBreaks]}
+                                >{blog?.body.replace(/<br>/gi, "&nbsp; \n")}</ReactMarkdown>
+                            </section>
+                        </div>
+                    </CenteredWrapper>
+                )}
+            </Await>
+        </React.Suspense>
     )
 }
 
